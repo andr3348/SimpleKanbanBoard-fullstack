37470a4df1902f93a9afde3e42bb32bf762bae12
andr33 <andree1211val@gmail.com>
2026-05-05 23:29:19 -0500

    chore: Initialize cookieParser for app to detect JWT as http-only cookie

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser()); // Allows app to detect JWT as a HTTP cookie
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
