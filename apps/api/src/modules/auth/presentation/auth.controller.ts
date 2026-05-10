626d08573bc10c045090dd19a7f2cdd91268ee68
andr33 <andree1211val@gmail.com>
2026-05-10 02:03:13 -0500

    feat: Implement /me endpoint

--- apps/api/src/modules/auth/presentation/auth.controller.ts
+++ apps/api/src/modules/auth/presentation/auth.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { RegisterUseCase } from '../application/use-cases/register.use-case';
 import { LoginUseCase } from '../application/use-cases/login.use-case';
@@ -12,12 +14,19 @@ import { RegisterDto } from '../application/dtos/register.dto';
 import { LoginDto } from '../application/dtos/login.dto';
 import { type Response } from 'express';
 import { AuthResponseDto } from './dtos/auth-response.dto';
+import {
+  type AuthenticatedUser,
+  CurrentUser,
+} from 'src/common/decorators/current-user.decorator';
+import { GetUserUseCase } from 'src/modules/user/application/use-cases/get-user.usecase';
+import { JwtAuthGuard } from '../infrastructure/guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly registerUser: RegisterUseCase,
     private readonly loginUser: LoginUseCase,
+    private readonly getUser: GetUserUseCase,
   ) {}
 
   @Post('register')
@@ -46,6 +55,13 @@ export class AuthController {
   logout(@Res({ passthrough: true }) res: Response) {
     res.clearCookie('access_token');
     return { message: 'Logged out successfully' };
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  async me(@CurrentUser() user: AuthenticatedUser): Promise<AuthResponseDto> {
+    const currentUser = await this.getUser.execute(user.id);
+    return AuthResponseDto.fromEntity(currentUser);
   }
 
   private setTokenCookie(res: Response, token: string) {
